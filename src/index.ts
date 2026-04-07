f48252cbfe076b931cce51ba2e70a012d4c660fe
asionsolver <asionsolver274@gmail.com>
2026-04-07 13:32:03 +0600

    feat: add module pattern implementation for inventory management

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 // import "./hello";
-// import "./namespace/mathLib";
 
 // Es Module Example
-import "./namespace/es-module/payment/app";
+// import "./namespace/es-module/payment/app";
+
+// Namespace Example
+// import "./namespace/mathLib";
+// import "./namespace/validation";
+
+// Module Pattern Example
+import "./module-pattern/module";
